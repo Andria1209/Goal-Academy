d470f37ac5c7e6d11a61f3b98cbd65d9927f1387
Andria1209 <163862316+Andria1209@users.noreply.github.com>
2024-03-28 18:29:10 +0100

    commit

---
+++ day 04/homework/task1.py
@@ -0,0 +1,4 @@
+name=input("enter your name: ")
+surname =input("enter your surname: ")
+age = input("enter your age: ")
+print("Chemi saxelia:", name, "Chemi gvaria: ", surname, "Chemi asakia: ", age)
