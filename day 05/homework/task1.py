9ff1311211288c41f036bf81ebe2e7fad4d0b9ce
Andria1209 <163862316+Andria1209@users.noreply.github.com>
2024-03-28 18:56:14 +0100

    commit

---
+++ day 05/homework/task1.py
@@ -0,0 +1,12 @@
+name = andria        #here isn't quotation marks
+print(name           #here isn't backet/nails
+surname = "nikoladze"
+age = 14
+print(surname + age) #we can't plus  string with interger
+print(andria)        #here isn't quotation marks
+age1 = 17
+age2 = "18"
+print(age1 + age2)   #we can't plus  string with integer
+317key = 100         #we can't start number to create variable
+
+
