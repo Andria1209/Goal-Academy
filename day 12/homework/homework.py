4d48852dc5ea5693f4524a0b4e57df8076c14ed8
Andria1209 <163862316+Andria1209@users.noreply.github.com>
2024-04-04 22:14:31 +0200

    commit

---
+++ day 12/homework/homework.py
@@ -0,0 +1,26 @@
+variable = input("enter your string type: ")
+variable2= int(input("enter your integer type: "))
+variable3= float(input("enter your float type: "))
+variable4= bool(input("enter your boolean type: "))
+print(variable)
+print(variable2)
+print(variable3)
+print(variable4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day 13/homework/homework.py
@@ -0,0 +1,28 @@
+name = "andria"
+age = 15
+print(name == "andri" or age == 13)
+variable1="andria niko"
+number=18
+print(variable1 == "andria niko" and number == 17)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
