1c1fa05aab1f8ca61a540b3c359a46bc5fba5a33
Andria1209 <163862316+Andria1209@users.noreply.github.com>
2024-04-15 19:12:27 +0200

    commit

---
+++ day 20/homework/homework.py
@@ -0,0 +1,9 @@
+num1 = 15>5
+num2 = 15<5
+print(num1 or num2)
+print(num2 and num1)
+
+
+
+
+
---
+++ day 20/homework/homework1.py
@@ -0,0 +1,7 @@
+temperature = 14
+print(temperature > 5)
+print(temperature >= 15)
+print(temperature < 10)
+print(temperature <= 15)
+print(temperature == 15)
+print(temperature == 14)
